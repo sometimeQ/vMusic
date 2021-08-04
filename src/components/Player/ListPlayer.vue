3f8a6312411f8ae1e5a90d14afd6d3176be14533
One <494231599@qq.com>
2021-08-04 23:11:44 +0800

    someOne|歌单列表布局

--- src/components/Player/ListPlayer.vue
+++ src/components/Player/ListPlayer.vue
@@ -17,19 +17,29 @@
                     <ul>
                         <li class="item">
                             <div class="item-left">
-                                <div></div>
+                                <div class="item-play"></div>
                                 <p>mmmmmmmmmm</p>
                             </div>
                             <div class="item-right">
-                                <div></div>
-                                <div></div>
+                                <div class="item-favorite"></div>
+                                <div class="item-del"></div>
+                            </div>
+                        </li>
+                        <li class="item">
+                            <div class="item-left">
+                                <div class="item-play"></div>
+                                <p>看看这是什么情况啊</p>
+                            </div>
+                            <div class="item-right">
+                                <div class="item-favorite"></div>
+                                <div class="item-del"></div>
                             </div>
                         </li>
                     </ul>
                 </ScrollView>
             </div>
             <div class="player-bottom">
-                <p>关闭</p>
+                <p @click.stop="hidden">关闭</p>
             </div>
         </div>
     </div>
@@ -39,7 +49,12 @@
 import ScrollView from '../ScrollView'
 export default {
     name: 'ListPlayer',
-    components: { ScrollView }
+    components: { ScrollView },
+    methods: {
+        hidden () {
+            console.log('点击啦关闭按钮')
+        }
+    }
 }
 </script>
 
@@ -53,7 +68,7 @@ export default {
         position: fixed;
         left: 0;
         bottom: 0;
-        @include bg_color;
+        @include bg_sub_color;
         .player-wrapper{
             // 上
             .player-top{
@@ -103,9 +118,62 @@ export default {
                 .item{
                     height: 100%;
                     border-top: 1px solid #cccccc;
+                    border-bottom: 1px solid #cccccc;
                     display: flex;
                     justify-content: space-between;
-
+                    padding: 20px 40px;
+                    box-sizing: border-box;
+                    .item-left{
+                        width: 70%;
+                        display: flex;
+                        align-items: center;
+                        .item-play{
+                            width: 56px;
+                            height: 56px;
+                            margin-right: 20px;
+                            @include bg_img('../../assets/images/small_play')
+                        }
+                        p{
+                            width: 80%;
+                            @include font_size($font_medium_s);
+                            @include font_color;
+                            @include no-wrap;
+                        }
+                    }
+                    .item-right{
+                        display: flex;
+                        align-items: center;
+                        .item-favorite{
+                            width: 56px;
+                            height: 56px;
+                            @include bg_img('../../assets/images/small_un_favorite');
+                            // 激活状态下的图片
+                            &.active{
+                                @include bg_img('../../assets/images/small_favorite');
+                            }
+                        }
+                        .item-del{
+                            width: 70px;
+                            height: 70px;
+                            margin-left: 20px;
+                            margin-right: -30px;
+                            box-sizing: border-box;
+                            @include bg_img('../../assets/images/small_del');
+                        }
+                    }
+                }
+            }
+            // 底部的关闭布局
+            .player-bottom{
+                width: 100%;
+                height: 100px;
+                line-height: 100px;
+                @include bg_color;
+                p{
+                    text-align: center;
+                    color: #ffffff;
+                    @include no-wrap;
+                    @include font_size($font_medium);
                 }
             }
         }
